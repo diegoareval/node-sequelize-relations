05b3c098da5f0b2aaa51f3b942fd4291aaa89640
Diego <diego2000avelar@gmail.com>
2021-04-09 20:21:59 -0600

    defining models and migrations

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,23 +1,26 @@
-const userController = require('../controller').user;
-const postController = require('../controller').post;
+const Student = require('../models').Student;
+const Classroom = require('../models').Classroom;
+const Course = require('../models').Course;
 module.exports = (app) => {
 
     app.get('/api',(req,res) => {
-        res.status(200).send({
-            data : "Welcome Node Sequlize API v1"
+        return Student
+        .findAll({
+          include: [{
+            model: Classroom,
+            as: 'classroom'
+          },{
+            model: Course,
+            as: 'courses'
+          }],
+          order: [
+            ['createdAt', 'DESC'],
+            [{ model: Course, as: 'courses' }, 'createdAt', 'DESC'],
+          ],
         })
+        .then((students) => res.status(200).send(students))
+        .catch((error) => { res.status(400).send(error); });
     })
 
-    app.get('/api/users',userController.getAllUsers);
-
-    app.post('/api/user/create',userController.create);
-
-    app.put('/api/user/:userId',userController.update);
-
-    app.get('/api/:userId/posts',postController.getAllPostsOfUser);
-
-    app.post('/api/post/create',postController.createPost);
-
-    app.put('/api/:postId',postController.update);
-
+   
 }
